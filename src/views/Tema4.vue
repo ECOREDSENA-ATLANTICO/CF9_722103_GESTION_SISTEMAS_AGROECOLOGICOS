f34a263c0a6a2a1863c4254aad533adfce13d7bb
acearaujo <hola@alvaroaraujo.com>
2024-06-07 15:34:18 -0500

    Enlace de Video Tema4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -104,7 +104,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/juXtqVqmMeo?si=YPPfvsx1x05rQbNp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     figure.mb-5
       img.img-t.d-inline-block.align-middle.me-2(src='@/assets/curso/tema2/img25.svg', alt='')
